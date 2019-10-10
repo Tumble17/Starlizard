cbb4a0a55cd14aec69b37fd2334ad75e9136dbfc
Tumble17 <dyerj1710@googlemail.com>
2019-10-10 21:56:48 +0000

    Attaching Docker Compose code

---
+++ compose-pipeline/json_producer.py
@@ -0,0 +1,30 @@
+import kafka
+import json
+import random
+from collections import OrderedDict
+
+producer = kafka.KafkaProducer(bootstrap_servers=['localhost:9092'])
+
+WHILE_LOOP_INDEX = 0
+GLOBAL_SEED  = 42
+SEEDING_FLAG = False
+TEST_MODE = True
+
+while WHILE_LOOP_INDEX < 1000:
+    
+    if SEEDING_FLAG:
+        
+        random.seed(GLOBAL_SEED); random_three_digit_match = random.randint(100, 999)
+        random.seed(GLOBAL_SEED); random_price = round(random.uniform(1.0, 10.0), 2)
+
+    else:
+        random_three_digit_match = random.randint(100, 999)
+        random_price = round(random.uniform(1.0, 10.0), 2)
+
+    json_message = json.dumps(OrderedDict([("match", random_three_digit_match), ("price", random_price)]))
+
+    if TEST_MODE:
+        WHILE_LOOP_INDEX += 1
+
+    print(json_message)
+
