47a0582613fc7fd40ea4cdc429173abf2c4e261e
Tumble17 <dyerj1710@googlemail.com>
2019-10-14 08:24:12 +0000

    Added the server location to the consumer script

--- compose-pipeline/kafka_consumer.py
+++ compose-pipeline/kafka_consumer.py
@@ -1,7 +1,18 @@
 import kafka
+import json
+
+TEST_MODE = False
 
 def consume():
 
-    consumer = kafka.KafkaConsumer('prices-topic', auto_offset_reset='earliest')
-    for message in consumer:
-        print(message)
+    consumer = kafka.KafkaConsumer('prices-topic', 
+            bootstrap_servers=['localhost:9092'])
+            #auto_offset_reset='earliest')
+
+    while not TEST_MODE:
+        
+        # Print out consumer messages from Kafka broker on topic
+        for message in consumer:
+            print(message.value.decode('utf-8'))
+
+consume()
