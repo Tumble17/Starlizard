05e013d72fefb73508a87b98c605348a377173b1
Jack Dyer <jack.dyer@ee.co.uk>
2019-10-14 09:33:46 +0100

    Adding code standards to producer
    Part way through adding in comments and parsers

--- compose-pipeline/json_producer.py
+++ compose-pipeline/json_producer.py
@@ -1,20 +1,27 @@
+# =============================================
+#               PACKAGES
+# =============================================
 import kafka
 import json
 import random
 from collections import OrderedDict
 
-producer = kafka.KafkaProducer(bootstrap_servers=['localhost:9092'], 
-                               value_serializer=lambda x: x.encode('utf-8'))
-
+# =============================================
+#              GLOBAL FIELDS
+# =============================================
 WHILE_LOOP_INDEX = 0
 GLOBAL_SEED  = 42
 SEEDING_FLAG = False
 TEST_MODE = False
 
+# =============================================
+#               FUNCTIONS
+# =============================================
+
 while WHILE_LOOP_INDEX < 1000:
-    
+
     if SEEDING_FLAG:
-        
+
         random.seed(GLOBAL_SEED); random_three_digit_match = random.randint(100, 999)
         random.seed(GLOBAL_SEED); random_price = round(random.uniform(1.0, 10.0), 2)
 
@@ -23,11 +30,10 @@ while WHILE_LOOP_INDEX < 1000:
         random_price = round(random.uniform(1.0, 10.0), 2)
 
     json_message = json.dumps(OrderedDict([("match", random_three_digit_match), ("price", random_price)]))
-    
+
     # Attempt to send message to the Kafka topic
     producer.send('prices-topic', json_message)
 
     if TEST_MODE:
         WHILE_LOOP_INDEX += 1
         print(json_message)
-
