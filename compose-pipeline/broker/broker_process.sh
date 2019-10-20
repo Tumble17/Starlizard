9c355637c2cba212930e281f1590c4e9eca9f103
Jack Dyer <jack.dyer@ee.co.uk>
2019-10-20 20:20:18 +0100

    Combine zookeeper and kafka process commands into the broker process

--- compose-pipeline/broker/broker_process.sh
+++ compose-pipeline/broker/broker_process.sh
@@ -5,8 +5,14 @@ export KAFKA_LOCATION=/home/kafka/kafka
 
 echo "$PROCESS: Starting process"
 
+echo "$PROCESS: Stopping lingering zookeeper server"
+. $KAFKA_LOCATION/bin/zookeeper-server-stop.sh
+
+echo "$PROCESS: Starting fresh zookeeper server"
+. $KAFKA_LOCATION/bin/zookeeper-server-start.sh $KAFKA_LOCATION/config/zookeeper.properties &
+
 echo "$PROCESS: Starting Kafka server"
-. $KAFKA_LOCATION/bin/kafka-server-start.sh $KAFKA_LOCATION/config/server.properties
+. $KAFKA_LOCATION/bin/kafka-server-start.sh $KAFKA_LOCATION/config/server.properties &
 
 echo "$PROCESS: Creating topic"
 . $KAFKA_LOCATION/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic prices-topic
